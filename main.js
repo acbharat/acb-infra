504a40231a081f66f5b45c61faf4ac659607fcac
acbharat <acbharat@gmail.com>
2018-09-17 14:34:14 -0400

    initial changes

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hi Folks,Welcome to world of Container Orchestration\n");
+  response.end("Welcome to world of Container Orchestration\n");
 });
 
 // listen on localhost:8000
